1f09a70073f69439f19e8733fd75536a56424994
Makadown <maseflores79@gmail.com>
2019-06-01 14:34:42 -0500

    Metiendo paddings

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -2,19 +2,35 @@ import 'package:flutter/material.dart';
 import '../models/image_model.dart';
 
 class ImageList extends StatelessWidget {
-    final List<ImageModel> images;
-    ImageList( this.images );
+  final List<ImageModel> images;
+  ImageList(this.images);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Container(
-          margin: EdgeInsets.all(20.0),
-          child: Image.network(images[index].url),
-          );
+        return buildImage(images[index]);
       },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      margin: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            child: Image.network(image.url),
+            padding: EdgeInsets.only(bottom: 8.0),
+          ),
+          Text(image.title)
+        ],
+      ),
     );
   }
 }
